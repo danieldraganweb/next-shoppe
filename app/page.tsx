77b629851df648cd06daef8a09fd502eaa3979e9
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-01-04 18:02:33 +0100

    Product display in container

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
+import styles from "./page.module.css";
 
 const Homepage = () => {
   const [count, setCount] = useState(1);
+  const [products, setProducts] = useState<any>([]);
+
+  const getData = async () => {
+    const data = await fetch("https://fakestoreapi.com/products?limit=5");
+    const json = await data.json();
+    setProducts(json);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(products);
 
   return (
     <div>
       <Navbar />
+      {products.map((product: any) => (
+        <div className={styles.productCard} key={product?.id}>
+          <h3>{product?.title}</h3>
+          <p>Price: {product?.price}</p>
+          <img
+            className={styles.productImage}
+            src={product?.image}
+            alt={product?.title}
+          />
+        </div>
+      ))}
       <h1>Homepage - - {count}</h1>
       <button onClick={() => setCount(count + 1)}>Click me</button>
 
